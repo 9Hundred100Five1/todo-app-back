a2ac9b9db860ed7ae2e89c28ad5d9c4c2f6eb51c
9Hundred100Five1 <jeyoung.james@gmail.com>
2022-09-04 01:01:46 +0900

    feat: add columns

--- app/models/user.py
+++ app/models/user.py
@@ -5,8 +5,9 @@ from app.db.database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    password= Column(String, nullable=False)
+    email = Column(String, primary_key=True, unique=True, index=True, nullable=False)
+    password= Column(String, primary_key=True, nullable=False)
+    full_name=Column(String, primary_key=True, nullable=False)
     sns_type = Column(String, primary_key=True, index=True)
 
     items = relationship("Item", back_populates="owner")
@@ -16,7 +17,7 @@ class Item(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
+    title = Column(String, primary_key=True, index=True)
     description = Column(String, index=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
 
