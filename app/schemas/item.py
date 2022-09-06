675f948761bddfe8a8ab0dd2fa96e1a9af405db5
9Hundred100Five1 <jeyoung.james@gmail.com>
2022-09-06 23:18:47 +0900

    fix: add item id

--- app/schemas/item.py
+++ app/schemas/item.py
@@ -4,9 +4,10 @@ from pydantic import BaseModel
 
 
 class ItemBase(BaseModel):
-    title: Optional[str]
+    title: str
     desc : Optional[str]
     id : int
+    owner_id: int
 
     class Config:
         orm_mode = True
