88c4fb3a7804faefae3c3964a0392b481909aec5
9Hundred100Five1 <jeyoung.james@gmail.com>
2022-09-06 23:15:40 +0900

    chore: edit router tags

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import items, login, users
+from app.api.endpoints import items, signin, users, signup
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
+api_router.include_router(signin.router, tags=["signin"])
+api_router.include_router(signup.router, tags=["signup"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
