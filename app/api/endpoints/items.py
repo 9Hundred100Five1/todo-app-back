1abb02ca2fa2ddec6abfec9e17af8d26ebacd827
9Hundred100Five1 <jeyoung.james@gmail.com>
2022-09-06 23:15:56 +0900

    feat: add item endpoint

--- app/api/endpoints/items.py
+++ app/api/endpoints/items.py
@@ -1,8 +1,74 @@
-from typing import Any, List
+from datetime import datetime, timedelta
+from typing import Any, Union
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from app.db.database import SessionLocal, engine
+from app.utils.hashing import Hasher
 from sqlalchemy.orm import Session
 
-from app import crud, models, schemas
+from pytz import timezone
+
+from app.models import models 
+from app.schemas import item, users
+from app.crud import crud
+from jose import JWTError, jwt
+
+SECRET_KEY = "6bf11ba31884f6136eb2025676851c2a9625e0e79d4629eb6f37587a2e66accb"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+models.Base.metadata.create_all(bind=engine)
 
 router = APIRouter()
+        
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/create", status_code=200)
+async def create(user_info: users.UserBase, item_info: item.ItemCreate, db: Session = Depends(get_db)):
+    if not item_info.title or item_info.desc:
+        return JSONResponse(status_code=400, content=dict(detail="Title and Description must be provided'"))
+    elif not user_info.email or not user_info.password or not user_info.full_name:
+        return JSONResponse(status_code=400, content=dict(detail="Email, PW and Full name must be provided'"))
+    now = datetime.now(timezone('Asia/Seoul'))
+    crud.create_user_item(db, item_info, user_info.email)
+    return JSONResponse(status_code=200, content=dict({"title": item_info.title, "desc": item_info.desc, "created_at": now}))
+
+@router.delete("/delete", status_code=200)
+async def delete(user_info: users.UserBase, item_info: item.ItemCreate, db: Session = Depends(get_db)):
+    if not item_info.title or item_info.desc:
+        return JSONResponse(status_code=400, content=dict(detail="Title and Description must be provided'"))
+    elif not user_info.email or not user_info.password or not user_info.full_name:
+        return JSONResponse(status_code=400, content=dict(detail="Email, PW and Full name must be provided'"))
+    
+    is_exist = crud.get_item(db, item_info.id, item_info.owner_id)
+
+    if not is_exist:
+        return JSONResponse(status_code=400, content=dict(detail="NO_MATCH_ITEM"))
+    
+    crud.del_item(db, item_info.id, item_info.owner_id)
+    return JSONResponse(status_code=200, content=dict("SUCCESS"))
+
+@router.post("/fetch", status_code=200)
+async def fetch(user_info: users.UserBase, item_info: item.ItemCreate, db: Session = Depends(get_db)):
+    if not item_info.title or item_info.desc:
+        return JSONResponse(status_code=400, content=dict(detail="Title and Description must be provided'"))
+    elif not user_info.email or not user_info.password or not user_info.full_name:
+        return JSONResponse(status_code=400, content=dict(detail="Email, PW and Full name must be provided'"))
+    crud.get_item(db, item_info.id, item_info.owner_id)
+    return JSONResponse(status_code=200, content=dict({"title": item_info.title, "desc": item_info.desc, "created_at": "UNKNOWN"}))
